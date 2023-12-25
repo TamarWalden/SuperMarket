3e450b8f8e197abb5fbb677862d96606a70f7eab
TamarWalden <t0527149764@gmail.com>
2023-12-25 03:10:23 +0200

    first commit

---
+++ source/repos/SuperMarket/SuperMarket/Controllers/ProductController.cs
@@ -0,0 +1,48 @@
+﻿using Common_DTO.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Service_BLL.Interfaces;
+
+namespace SuperMarket.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        readonly IProductService productService;
+        private ILogger<string> logger;
+        public ProductController(IProductService service, ILogger<string> logger)
+        {
+            productService = service;
+            this.logger = logger;
+        }
+
+        [HttpGet]
+        public async Task<ProductDTO> Get(int code)
+        {
+            try
+            {
+                return await productService.GetByIdAsync(code);
+            }
+            catch (Exception ex)
+            {
+                logger.LogError("faild in api to add product" + ex.Message);
+                throw ex;
+            }
+        }
+        // POST api/<ValuesController>
+        [HttpPost]
+        public async Task AddNewAssignment(ProductDTO newProduct)
+        {
+            try
+            {
+                await productService.AddNewProductAsync(newProduct);
+            }
+            catch (Exception ex) 
+            {
+                logger.LogError("faild in api to add product" + ex.Message);
+            }
+        }
+    }
+}
